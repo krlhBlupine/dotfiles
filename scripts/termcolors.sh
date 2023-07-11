d5cedf5e78feab6d8f89e9df33d8529df1864ffc
Abaan404 <67100191+Abaan404@users.noreply.github.com>
2023-07-11 15:19:06 +0300

    style edits

--- scripts/termcolors.sh
+++ scripts/termcolors.sh
@@ -2,3 +2,4 @@
 wallpaper=$1
 echo '#Setting wallpaper to scheme  matching '$1'.' > /home/olive/.config/kitty/colors.conf
 /home/olive/go/bin/schemer2 --format img::kitty -minBright 60 -in $1 >> /home/olive/.config/kitty/colors.conf
+kill -SIGUSR1 $(pgrep kitty)
